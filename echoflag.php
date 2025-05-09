033f9facc1e562ffdf50f327e4953ff0873d9640
DocteurMoriarty <128460992+DocteurMoriarty@users.noreply.github.com>
2025-05-09 10:50:10 +0200

    Update echoflag.php

--- echoflag.php
+++ echoflag.php
@@ -1,6 +1,14 @@
-<body>
-  <?php 
-  echo "Hello, world Sacha!"; 
-  echo file_get_contents("flag.txt");
-  ?>
-</body>
+<?php
+$dir = isset($_GET['path']) ? $_GET['path'] : '.';
+
+if (is_dir($dir)) {
+    echo "<h2>Listing of: " . htmlspecialchars($dir) . "</h2><ul>";
+    foreach (scandir($dir) as $file) {
+        if ($file === '.') continue;
+        echo "<li>" . htmlspecialchars($file) . "</li>";
+    }
+    echo "</ul>";
+} else {
+    echo "Invalid directory: " . htmlspecialchars($dir);
+}
+?>
